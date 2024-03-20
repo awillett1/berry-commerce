edf4fa7eb97e44035b6dc23fc20b8aae07c5beca
strzy <happyth0ug5ts@gmail.com>
2024-03-19 22:12:17 -0400

    Update login.js

--- login.js
+++ login.js
@@ -63,9 +63,3 @@ async function handleRegistration(event) {
         }
     }
 }
-
-function showRegistrationForm() {
-    // Toggle the display of the registration section
-    document.getElementById('roleSelectionForm').style.display = 'none';
-    document.getElementById('registrationSection').style.display = 'block';
-}

fd7dc954f9c9d990e915de9f09de4a3cdd00e668
strzy <happyth0ug5ts@gmail.com>
2024-03-21 18:54:36 -0400

    Changed it to storage link instead

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,23 +1,51 @@
 // firebaseInit.js
 
-async function firebaseInit() {
+// Initialize Firebase
+// test comment
+/*
+async function initializeFirebase() {
     try {
         // Fetch Firebase configuration from the server
-        const response = await fetch('https://berry-commerce-default-rtdb.firebaseio.com/appConfigurations/firebaseConfig.json'); // Replace with your Firebase config file path
+        const response = await fetch('https://berry-commerce-default-rtdb.firebaseio.com/appConfigurations/firebaseConfig.json');
         const firebaseConfig = await response.json();
 
         // Initialize Firebase with the fetched configuration
         firebase.initializeApp(firebaseConfig);
 
         console.log('Firebase initialized successfully.');
-
-        return Promise.resolve(); // Resolve the promise when initialization is done
     } catch (error) {
         console.error('Error initializing Firebase:', error);
-        return Promise.reject(error); // Reject the promise if an error occurs
+        // Handle errors, e.g., prevent further execution or show an error message
     }
 }
 
 // Call the function to initialize Firebase when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', firebaseInit);
+document.addEventListener('DOMContentLoaded', initializeFirebase);
+*/
+
+// Initialize Firebase
+async function initializeFirebase() {
+    try {
+        // Fetch Firebase configuration link from Firebase Storage
+        const storageRef = firebase.storage().ref();
+        const configFileRef = storageRef.child('firebaseConfig.json');
+        const response = await configFileRef.getDownloadURL();
+        const firebaseConfigLink = await response.json();
+        
+        // Fetch Firebase configuration from the link
+        const configResponse = await fetch(firebaseConfigLink.firebaseConfigLink);
+        const firebaseConfig = await configResponse.json();
+
+        // Initialize Firebase with the fetched configuration
+        firebase.initializeApp(firebaseConfig);
+
+        console.log('Firebase initialized successfully.');
+    } catch (error) {
+        console.error('Error initializing Firebase:', error);
+        // Handle errors, e.g., prevent further execution or show an error message
+    }
+}
+
+// Call the function to initialize Firebase when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', initializeFirebase);
 

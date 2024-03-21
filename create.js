8bb9571c56d9b55ef45e7ee67884bf7db9ba3483
strzy <happyth0ug5ts@gmail.com>
2024-03-20 22:36:25 -0400

    Update create.js

--- create.js
+++ create.js
@@ -122,7 +122,7 @@ function showRegistrationForm() {
 
 document.addEventListener('DOMContentLoaded', function () {
     const nextButton = document.getElementById('nextButton');
-    nextButton.addEventListener('click', handleRoleSelection);
+    nextButton.addEventListener('click', showRegistrationForm);
 });
 
 async function showRegistrationForm() {

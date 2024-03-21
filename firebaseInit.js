4bb7b9a24af3a75fbb4aba1e73cb62e871dec9a3
strzy <happyth0ug5ts@gmail.com>
2024-03-21 19:21:41 -0400

    Update firebaseInit.js

--- firebaseInit.js
+++ firebaseInit.js
@@ -2,7 +2,7 @@
 
 // Initialize Firebase
 // test comment
-/*
+
 async function initializeFirebase() {
     try {
         // Fetch Firebase configuration from the server
@@ -21,32 +21,3 @@ async function initializeFirebase() {
 
 // Call the function to initialize Firebase when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', initializeFirebase);
-*/
-
-// Initialize Firebase using configuration from Firebase Storage
-async function initializeFirebase() {
-    try {
-      // Access Firebase Storage without initializing Firebase
-      const storage = firebase.storage();
-  
-      // Get a reference to the firebaseConfig.json file in Firebase Storage
-      const storageRef = storage.ref().child('firebaseConfig.json');
-  
-      // Get the download URL of the firebaseConfig.json file
-      const downloadURL = await storageRef.getDownloadURL();
-  
-      // Fetch Firebase configuration from the URL
-      const configResponse = await fetch(downloadURL);
-      const firebaseConfig = await configResponse.json(); // Assuming firebaseConfig.json is in JSON format
-  
-      // Initialize Firebase with the fetched configuration
-      firebase.initializeApp(firebaseConfig);
-      console.log('Firebase initialized successfully.');
-    } catch (error) {
-      console.error('Error initializing Firebase:', error);
-      // Handle errors, e.g., prevent further execution or show an error message
-    }
-  }
-  
-  // Call the function to initialize Firebase when the DOM content is loaded
-  document.addEventListener('DOMContentLoaded', initializeFirebase);

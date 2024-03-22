c39c4bb08dbe5aee32e13c95667e9266f316c3dd
strzy <happyth0ug5ts@gmail.com>
2024-03-21 21:30:29 -0400

    added new ids

--- email.js
+++ email.js
@@ -1,35 +1,31 @@
 // Function to handle email change
-function changeEmail(newEmail) {
-    var user = firebase.auth().currentUser;
-  
-    if (user) {
+function changeEmail() {
+  var newEmail = document.getElementById('newEmail').value;
+  var user = firebase.auth().currentUser;
+
+  if (user) {
       user.updateEmail(newEmail).then(function() {
-        // Update email in 'users' collection
-        updateUserEmail(user.uid, newEmail)
-          .then(function() {
-            console.log("User email updated successfully.");
-            alert("Your email has been successfuly updated, your new email is", newEmail);
-  
-            // Update email in 'sellers' collection
-            updateSellerEmail(user.uid, newEmail)
+          // Update email in 'users' collection
+          updateUserEmail(user.uid, newEmail)
               .then(function() {
-                console.log("Seller email updated successfully.");
-                alert("Your email has been successfuly updated, your new email is", newEmail);
+                  console.log("User email updated successfully.");
+
+                  // Update email in 'sellers' collection
+                  updateSellerEmail(user.uid, newEmail)
+                      .then(function() {
+                          console.log("Seller email updated successfully.");
+                      })
+                      .catch(function(error) {
+                          console.error("Error updating seller email:", error);
+                      });
               })
               .catch(function(error) {
-                console.error("Error updating seller email:", error);
+                  console.error("Error updating user email:", error);
               });
-          })
-          .catch(function(error) {
-            alert("Email could not update.")
-            console.error("Error updating user email:", error);
-          });
       }).catch(function(error) {
-        alert("Email could not update.")
-        console.error("Error updating email:", error);
+          console.error("Error updating email:", error);
       });
-    } else {
+  } else {
       console.error("No user signed in.");
-    }
   }
-  
+}

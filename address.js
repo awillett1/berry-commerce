51d6a3cb12d0e1e0f2b3ffa318bf250d9b69a513
strzy <happyth0ug5ts@gmail.com>
2024-03-27 21:04:40 -0400

    fixed some formatting

--- address.js
+++ address.js
@@ -77,40 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('userIcon').addEventListener('click', handleUserIconClick);
 });
 
-/*
-// Function to check login status and redirect accordingly
-function checkLoginAndRedirect() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in
-            const firestore = firebase.firestore();
-            const userRef = firestore.collection('users').doc(user.uid);
-
-            userRef.get().then(function(doc) {
-                if (doc.exists) {
-                    const userData = doc.data();
-                    const userRole = userData.role;
-
-                    // Redirect to appropriate page based on user role
-                    if (userRole === 'user') {
-                        window.location.href = 'account.html';
-                    } else if (userRole === 'seller') {
-                        window.location.href = 'seller.html';
-                    }
-                } else {
-                    console.log('No user data found!');
-                    // Handle case where user data does not exist
-                }
-            }).catch(function(error) {
-                console.log('Error getting user data:', error);
-            });
-        } else {
-            // No user is signed in
-            window.location.href = 'login.html';
-        }
-    });
-}
-*/
 
 function checkLoginAndRedirect() {
     firebase.auth().onAuthStateChanged(function(user) {
